ba51e34bce037ba9ba176633dab6393a2ddf62fd
Jack Putt <jackputt2000@hotmail.co.uk>
2022-05-06 08:58:19 +0100

    register form added and updated styling on login as well

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -2,6 +2,7 @@ import React, { useState, Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './custom.css'
 import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => {
     const [token, setToken] = useState([]);
@@ -10,11 +11,15 @@ const App = () => {
 		//Grab values from login form
 		var { uname, pass } = document.forms[0];
 
+		await performLogin(uname.value, pass.value);
+		
+	}
+	const performLogin = async (username, pwd) => {
 		//Login in using api/login endpoint
 		await fetch('https://localhost:7263/api/login', {
 			method: "post",
 			headers: { 'Content-Type': 'application/json' },
-			body: '{ "username": "' + uname.value + '", "password": "' + pass.value + '" }'
+			body: '{ "username": "' + username + '", "password": "' + pwd + '" }'
 		})
 			.then(function (response) {
 				if (response.ok) {
@@ -29,12 +34,39 @@ const App = () => {
 			.catch(function (error) {
 				console.log("Error: ", error);
 			});
-	}
+	};
 
+	const register = async () => {
+		var { uname, email, pass } = document.forms[1];
+
+		//Login in using api/login endpoint
+		await fetch('https://localhost:7263/api/register', {
+			method: "post",
+			headers: { 'Content-Type': 'application/json' },
+			body: '{ "username": "' + uname.value + '", "email":"' + email.value + '", "password": "' + pass.value + '","confirmPassword": "' + pass.value + '" }'
+		})
+			.then(function (response) {
+				if (response.ok) {
+					console.log("Successfully registered!");
+					return response.json();
+				}
+				throw new Error(response.statusText);
+			})
+			.then(function () {
+				performLogin(uname.value, pass.value);
+            })
+			.catch(function (error) {
+				console.log("Error: ", error);
+			});
+    }
 	//If token not set yet, then show login/register page
     if (token.length == 0 || token.token == '') {
-        return (
-            <Login loginSubmit={loginSubmit} />
+		return (
+			<>
+				<Login loginSubmit={loginSubmit} />
+				<div className="separator"></div>
+					<Register register={register} />
+			</>
         );
     }
 
--- ClientApp/src/components/Register.js
+++ ClientApp/src/components/Register.js
@@ -3,27 +3,33 @@
 const onSubmit = (event) => {
     event.preventDefault();
 }
-const Login = (props) => {
+const Register = (props) => {
 
     return (
         <>
             <div className="form">
-                <h4>Login</h4>
+                <h4>Register</h4>
                 <form onSubmit={onSubmit}>
                     <div className="input-container">
                         <label className="w-100">Username </label>
                         <div className="w-100">
-                            <input type="text" name="uname" className="form-control" placeholder="Username..." required />
+                            <input type="text" className="form-control" name="uname" placeholder="Username..." required />
+                            </div>
+                    </div>
+                    <div className="input-container">
+                        <label className="w-100">Email </label>
+                        <div className="w-100">
+                            <input type="email" className="form-control" name="email" placeholder="Email..." required />
                         </div>
                     </div>
                     <div className="input-container">
                         <label className="w-100">Password </label>
                         <div className="w-100">
-                            <input type="password" name="pass" className="form-control" placeholder="Password..." required />
+                            <input type="password" className="form-control" name="pass" placeholder="Password..." required />
                         </div>
                     </div>
                     <div className="button-container">
-                        <input type="submit" onClick={() => props.loginSubmit()} />
+                        <input type="submit" onClick={() => props.register()} value="Register" />
                     </div>
                 </form>
             </div>
@@ -31,4 +37,4 @@ const Login = (props) => {
     );
 };
 
-export default Login;
+export default Register;
