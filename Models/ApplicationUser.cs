ef4bf1eab0632a4c70e649ac2a0af8626d81162d
Jack Putt <jackputt2000@hotmail.co.uk>
2022-05-06 02:13:46 +0100

    core configs setup and db models

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TvShowTracker.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
---
+++ Models/Series.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TvShowTracker.Models
+{
+    public class Series
+    {
+        [Key]
+        public string SeriesID { get; set; }
+        public string UserID { get; set; }
+        public string SeriesTitle { get; set; }
+        public string SeriesDescription { get; set; }
+        public string SeriesImage { get; set; }
+    }
+}
